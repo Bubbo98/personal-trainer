2369b0468f30722630a8cf60c2757a172319f446
Bubbo98 <jmaurizio@efebia.com>
2026-04-17 11:46:54 +0200

    Add Avambraccio and Cardio muscle groups to video management

--- src/components/admin/VideoManagement.tsx
+++ src/components/admin/VideoManagement.tsx
@@ -13,7 +13,7 @@ import {
 import { apiCall, formatDate, formatDuration } from '../../utils/adminUtils';
 import { Video, CreateVideoForm } from '../../types/admin';
 
-const MUSCLE_GROUPS = ['Polpaccio','Quadricipite','Femorale','Gluteo','Lombare','Dorsale','Trapezio','Pettorale','Spalle','Bicipite','Tricipite','Addome'];
+const MUSCLE_GROUPS = ['Polpaccio','Quadricipite','Femorale','Gluteo','Lombare','Dorsale','Trapezio','Pettorale','Spalle','Bicipite','Tricipite','Addome','Avambraccio','Cardio'];
 
 // Responsive page size: fewer cards on smaller screens
 const getPageSize = () => {
